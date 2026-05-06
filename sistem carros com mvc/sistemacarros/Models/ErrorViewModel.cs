b1972f5edd427959a3b725a039713e16a905c57f
LuizMiguel78 <149170949+LuizMiguel78@users.noreply.github.com>
2026-05-06 15:09:42 -0300

    sitema medico e carro

---
+++ sistem carros com mvc/sistemacarros/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sistemacarros.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
